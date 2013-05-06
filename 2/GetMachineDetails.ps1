f21b292673006fd6bb286ef311420918c6ecd1f8
AdamKorczynski <adamkorczynski81@gmail.com>
2013-05-06 21:49:12 +1000

    verbose and errors
    added verbose output, exception handling (not working yet), some
    validation and output formatting

--- 2/GetMachineDetails.ps1
+++ 2/GetMachineDetails.ps1
@@ -1,51 +1,121 @@
-﻿Function Get-MachineInfo
+﻿<#
+ 
+.SYNOPSIS
+This gets CPU and OS information about a machine
+ 
+.DESCRIPTION
+
+ 
+.EXAMPLE
+'COMPUTER1', 'localhost', 'SERVER1' | Get-MachineInfo
+
+
+'COMPUTER1', 'localhost', 'SERVER1' | Get-MachineInfo -Path Output.csv
+ 
+.NOTES
+Cannot reliably get NumberOfLogicalProcessors or NumberOfCores http://support.microsoft.com/kb/932370 
+ 
+.LINK
+ 
+#>
+Function Get-MachineInfo
 {
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory=$False, ValueFromPipeline=$True)]
-        [string] $ComputerName = 'localhost',
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [string] $ComputerName,
 
         [Parameter(Mandatory=$False)]
-        [string] $Path = 'Report.csv'
+        [string] $Path
     )
 
     begin {
         $MB = 1024 * 1024
         $GB = $MB * 1024
         $rows = @()
-    }
 
+        if($Path.Length -gt 0)
+        {
+            $dir = Split-Path $Path
+            if($dir.Length -gt 0)
+            {
+                if(-not (Test-Path $dir))
+                {
+                    Write-Error "Directory '$dir' doesn't exist"
+                }
+            }
+        }
+    }
+    
     process {
 
-        $sessionOption = New-CimSessionOption –Protocol DCOM
-        $session = New-CImSession -ComputerName $computerName -SessionOption $sessionOption
+        try
+        {
+            Write-Verbose "Perform operation 'Ping Computer' with following parameters, ''computerName' = $ComputerName"
+            Test-Connection $ComputerName -Count 1 | Out-Null
+            Write-Verbose "Operation 'Ping Computer' complete"
 
-        $procInfo = Get-CimInstance -CimSession $session -Class CIM_Processor | 
-        select "NumberOfProcessors", "NumberOfCores"
+        }
+        catch
+        {
+            Write-Error "Error Connecting to $ComputerName $_.Message"
+            return
+        }
 
+        try
+        {
+            Write-Verbose "Perform operation 'Establish CIM Session' with following parameters, ''ComputerName' = $ComputerName, 'protocol' = DCOM"
+            $sessionOption = New-CimSessionOption –Protocol DCOM
+            $session = New-CImSession -ComputerName $computerName -SessionOption $sessionOption
+            Write-Verbose "Operation 'Establish CIM Session' complete"
+
+        }
+        catch
+        {
+            Write-Error "ERROR: $($_.Exception.Message)"
+            return
+        }
+
+        Write-Verbose "Perform operation 'Retrieve system data'"
         $os = Get-CimInstance -CimSession $session -Class CIM_OperatingSystem | 
-        select "CSDVersion", "Caption", "OSType", "Version"
+        select Caption, Version, CSDVersion
 
-        $mem = Get-CimInstance -CimSession $session -Class CIM_ComputerSystem | 
-        select -ExpandProperty TotalPhysicalMemory
+        $csInfo = Get-CimInstance -CimSession $session -Class CIM_ComputerSystem | 
+        select TotalPhysicalMemory, NumberOfProcessors
+        Write-Verbose "Operation 'Retrieve system data' complete"
 
-        if($mem / $GB -gt 0) {
-            $mem = "{0:N0} GB" -f ($mem / $GB)
+
+        if($csInfo.TotalPhysicalMemory / $GB -gt 0) {
+            $Memory = "{0:N0} GB" -f ($csInfo.TotalPhysicalMemory / $GB)
         }
         else {
-            $mem = "{0:N0} MB" -f ($mem / $MB)
+            $Memory = "{0:N0} MB" -f ($csInfo.TotalPhysicalMemory / $MB)
         }
 
         $properties = @{
-            'Processors'= $procInfo;
-            'OS'= $os;
-            'Memory'= $mem;
+            'OSName'= "$($os.Caption) $($os.CSDVersion)"
+            'OSVersion' = $os.Version
+            'Memory'= $Memory
+            'Processors' = $csInfo.NumberOfProcessors
         }
-
-        $rows += New-Object –TypeName PSObject –Prop $properties
+        
+        $rows += New-Object –TypeName PSObject –Property $properties
     }
 
     end {
-    #output
+
+
+        Write-Verbose "$($rows.Length) device(s) found"
+
+        if($Path.Length -gt 0)
+        {
+                Write-Verbose "Writing output to $Path"
+                $rows | Export-Csv $Path -NoTypeInformation
+        }
+        else
+        {
+            $rows | Format-Table
+        }
+
     }
 }
